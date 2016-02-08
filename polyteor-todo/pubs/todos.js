d3470522cd002b4c3ab6ba2bed70d62ad138e299
Jonatan <jonatan.sombie@gmail.com>
2016-02-08 08:03:40 +0000

    Add Meteor Account management

--- polyteor-todo/pubs/todos.js
+++ polyteor-todo/pubs/todos.js
@@ -1,20 +1,7 @@
 SmartPub.smartPublish('todos', function () {
     return {
         selector:{
-            // $or: [
-            //     // {
-            //     //     private: false
-                    
-            //     // },
-            //     // // {
-            //     // //     private : {
-            //     // //         $exists: false
-            //     // //     }  
-            //     // // },
-            //     // {
-            //     //     creator: this.userId || Meteor.userId
-            //     // }
-            // ]
+         
         },
         coll: Todos,
         sort: {
