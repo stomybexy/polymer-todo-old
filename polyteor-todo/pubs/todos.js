44160d87145efd88080ef2ca1b7dc2f45092783b
Jonatan <jonatan.sombie@gmail.com>
2016-02-21 02:07:54 +0000

    Add meteor-subscription meteor-collection and meteor-object

--- polyteor-todo/pubs/todos.js
+++ polyteor-todo/pubs/todos.js
@@ -1,11 +1,36 @@
-SmartPub.smartPublish('todos', function () {
-    return {
-        selector:{
-         creator: this.userId || Meteor.userId()
-        },
-        coll: Todos,
-        sort: {
-            label: 1
+// SmartPub.smartPublish('todos', function () {
+//     return {
+//         selector:{
+         
+//         },
+//         coll: Todos,
+//         sort: {
+//             label: 1
+//         }
+//     }
+// })
+
+SmartPub.smartPublishComposite('todos', {
+    find: function(){
+        return {
+            coll: Todos,
+            sort: {
+                label: 1
+            }
         }
-    }
+    },
+    children: [
+        {
+            find: function(todo){
+                return {
+                    selector: {
+                        _id: todo.owner
+                    },
+                    coll: Meteor.users,
+                    single: true
+                }
+            },
+            name: 'user'
+        }
+    ]
 })
